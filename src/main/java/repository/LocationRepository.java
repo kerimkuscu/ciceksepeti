758a510d2a0dae30d1b9776213be5663c696da12
erkankarabulut <erkan_karabulut@hotmail.com.tr>
2019-03-22 23:02:52 +0300

    feat: add project structure

---
+++ src/main/java/repository/LocationRepository.java
@@ -0,0 +1,24 @@
+package repository;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LocationRepository extends BaseRepository {
+
+    /*public void saveLocation(int x, int y, int userId, int marketId){
+        Statement statement = null;
+
+        try {
+            this.con
+            statement = this.createStatement();
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            cleanResources(statement);
+        }
+
+    }  */
+
+}
